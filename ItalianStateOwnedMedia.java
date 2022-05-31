8cdf31cac984166b810d9d8da02a0d1de8d14b16
4ndrewC <84311342+4ndrewC@users.noreply.github.com>
2022-05-31 16:03:54 +0800

    Create ItalianStateOwnedMedia.java

---
+++ ItalianStateOwnedMedia.java
@@ -0,0 +1,8 @@
+public class ItalianStateOwnedMedia{
+  public static void main(String[] args){
+    System.out.println("Italy is cool");
+    int IQ = 0;
+    System.out.println("JC's IQ is " + IQ);
+    System.out.println("pen pineapple apple pen");
+  }  
+}
