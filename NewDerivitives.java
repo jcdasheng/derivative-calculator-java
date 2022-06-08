bc0df48b3d00b46d46cddccb72f02aea211d6cac
JC zhang <39557261+jcdasheng@users.noreply.github.com>
2022-06-08 16:07:24 +0800

    Add files via upload

--- NewDerivitives.java
+++ NewDerivitives.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 class Derivative {
 	
 	Function func;
@@ -19,7 +21,26 @@ class Derivative {
 		if(power == 0) {
 			temp  = "0";
 		}
+	
+		if(f.getF().equals("sin")){
+			temp = "cos";
+			Function derivative = new Function(temp);		
+			return derivative;
+			
+		}
 		
+		if(f.getF().equals("cos")){
+			temp = "-sin";
+			Function derivative = new Function(temp);		
+			return derivative;
+			
+		}
+		if(f.getF().equals("tan")){
+			temp = "sec^2";
+			Function derivative = new Function(temp);		
+			return derivative;
+			
+		}
 		if(f.getF().equals("sin(x)")){
 			temp = "cos(x)";
 			Function derivative = new Function(temp);		
@@ -37,8 +58,54 @@ class Derivative {
 			return derivative;
 			
 		}
+		if(f.getF().equals("tan(x)")){
+			temp = "sec(x)^2";
+			Function derivative = new Function(temp);		
+			return derivative;
+			
+		}
+		if(f.getF().equals("sin(x)")){
+			temp = "cos(x)";
+			Function derivative = new Function(temp);		
+			return derivative;
+			
+		}
+		if(f.getF().equals("cos(x)")){
+			temp = "-sin(x)";
+			Function derivative = new Function(temp);		
+			return derivative;
+			
+		}
+		if(f.getF().equals("tan(x)")){
+			temp = "sec(x)^2";
+			Function derivative = new Function(temp);		
+			return derivative;
+			
+		}
+		if(f.getF().equals("sec(x)")){
+			temp = "sec(x)tan(x)";
+			Function derivative = new Function(temp);		
+			return derivative;
+			
+		}
+		if(f.getF().equals("cot(x)")){
+			temp = "-sec(x)^2";
+			Function derivative = new Function(temp);		
+			return derivative;
+			
+		}
 		
-		
+		if(f.getF().equals("arcsin(x)")){
+			temp = "1/sqrt(1-x^2)";
+			Function derivative = new Function(temp);		
+			return derivative;
+			
+		}
+		if(f.getF().equals("sin^-1(x)")){
+			temp = "1/sqrt(1-x^2)";
+			Function derivative = new Function(temp);		
+			return derivative;
+		}
 		
 		for(int i = power; i > 2; i--) {
 			if(f.getCoef(i) != 0) {
@@ -196,7 +263,7 @@ class Derivative {
 				else
 					return Integer.parseInt( f.substring(index2, index1) );
 			}
-		}
+		}	
 		
 		return 0;
 	}
@@ -226,6 +293,7 @@ class Derivative {
 	
 	public static boolean isSign(char c) {
 		return c == '+' || c == '-';
+		
 	}
 	
 	
@@ -306,7 +374,7 @@ class QuotientRule{
 		}
 		
 	    public String getDerivative(){
-			Function test1 = new Function(in);
+		Function test1 = new Function(in);
 		Function test2 = new Function(ans);
 		Derivative first = new Derivative(test1);
 		Derivative second = new Derivative(test2);
@@ -321,12 +389,133 @@ class QuotientRule{
 	
 	
 }
+class ChainRule {
+			String in, ans;
+		public ChainRule(String input){
+			in = input;		
+			
+		}
+		
+	    public String getDerivative(){
+		
+		int index = in.indexOf("(");
+		int index1 = in.indexOf(")");
+		
+		String first = in.substring(0,index);
+		Function test1 = new Function(first);		
+		Derivative first1 = new Derivative(test1);
+		
+		
+		Function test2 = new Function(in.substring(index+1, index1));		
+		Derivative second = new Derivative(test2);
+		
+		return first1.findDerivative(test1).getF() + in.substring(index, index1) + ")" + "*" +  second.findDerivative(test2).getF();
+		
+		
+		
+		
+		
+		
+		
+		
+		}
+	
+	
+	
+}
+
+class ChainRule2 {
+			String in, ans;
+		public ChainRule2(String input){
+			in = input;		
+			
+		}
+		
+	    public String getDerivative(String inside){
+		
+		
+		if(inside.indexOf("(") == -1 ){
+			Function test3 = new Function(inside);		
+			Derivative second = new Derivative(test3);
+			return  "(" +second.findDerivative(test3).getF() + ")";
+		}
+		else {
+		int index = inside.indexOf("(");
+		int index1 = inside.lastIndexOf(")");
+		
+		String first = inside.substring(0,index);
+		Function test1 = new Function(first);		
+		Derivative first1 = new Derivative(test1);
+		
+		String second = inside.substring(index+1, index1);
+		//Function test2 = new Function(inside.substring(index+1, index1));		
+		//Derivative second = new Derivative(test2);
+		
+		return first1.findDerivative(test1).getF() + "(" +  second + ")*" +  getDerivative(second);
+			
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		}
+	
+	
+	
+}
 
 public class NewDerivitives{
 	
  public static void main(String[] args) {
+			//Constant rule
+			 Scanner scan = new Scanner(System.in);
+			 System.out.println("Please enter an equation for power rule");
+			 String str1 = scan.next();		
+			
+		
+			Function con = new Function(str1);
+			Derivative con1 = new Derivative(con);
+			System.out.println(con1.findDerivative(con).getF());
+			
+			
+			System.out.println("Please enter an equation for exponential rule");
+			 String str2 = scan.next();	
+			 Exponential test3 = new Exponential(str2);
+			System.out.println(test3.getDerivitive());
+			 
+			 
+			 System.out.println("Please enter an equation for Product rule");
+			 String str3 = scan.next();	
+			 String str4 = scan.next();	
+			Product test4 = new Product(str3, str4);
+			System.out.println(test4.getDerivitive());
+			
+			
+			 System.out.println("Please enter an equation for Quotient rule");
+			 String str5 = scan.next();
+			 String str6 = scan.next();
+			 QuotientRule test5 = new QuotientRule(str5, str6);
+			 System.out.println(test5.getDerivative());
+			 
+			 
+			  System.out.println("Please enter an equation for Chain rule");
+			 String str7 = scan.next();
+			 ChainRule2 test6 = new ChainRule2(str7);
+			System.out.println(test6.getDerivative(str7));
+			 
+			 /*
+			 System.out.println("Please enter an equation for power rule");
+			 String str1 = scan.next();	
+			
 			//Complete power rule
-			Function test1 = new Function("sin(x)");
+			Function test1 = new Function("cos(x)");
 			Derivative test2 = new Derivative(test1);
 			System.out.println(test2.findDerivative(test1).getF());
 			//Exponential rule 
@@ -336,12 +525,23 @@ public class NewDerivitives{
 			Product test4 = new Product("2x", "2x^5");
 			System.out.println(test4.getDerivitive());
 			//Quotient rule 
-			QuotientRule test5 = new QuotientRule("2x", "2x^5");
-			System.out.print(test5.getDerivative());
+			QuotientRule test5 = new QuotientRule("x", "sec(x)");
+			System.out.println(test5.getDerivative());
+			//Single chainRUle 
+			ChainRule test6 = new ChainRule("cos(x^2)");
+			System.out.println(test6.getDerivative());
+			//Multiple Chainrule 
+			
+			String in = "sin(cos(2x+1))";
+			
+			//System.out.println(in.substring(0, index));
+			
 			//Derivative test3 = test2.findDerivative(test1);
 			
+			ChainRule2 test7 = new ChainRule2("sin(cos(2x+1))");
 			
+			System.out.println(test7.getDerivative("sin(cos(sin(cos(2x+1))))"));
 			
-	
+	*/
 	}
 }
